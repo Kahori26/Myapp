f63081b47ebe5dcd43436774371323ccadec964d
Kahori26 <murakami.kahori994@mail.kyutech.jp>
2020-11-13 22:58:30 +0900

    写真フォルダを開けるようにした

--- lib/main.dart
+++ lib/main.dart
@@ -3,54 +3,85 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
-void main() => runApp(MyApp());
+void main() {
+ runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: MyHomePage(),
-    );
-  }
+ @override
+ Widget build(BuildContext context) {
+   return MaterialApp(
+     title: 'Hatch Demo',
+     theme: ThemeData(
+       primarySwatch: Colors.blue,
+       visualDensity: VisualDensity.adaptivePlatformDensity,
+     ),
+     home: HomePage(),
+   );
+ }
 }
 
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
+class HomePage extends StatefulWidget {
+ @override
+ _HomePageState createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  File _image;
-  final picker = ImagePicker();
+class _HomePageState extends State<HomePage> {
+ File _image;
+ final picker = ImagePicker();
 
-  Future getImage() async {
-    final pickedFile = await picker.getImage(source: ImageSource.camera);
+ Future getImageFromCamera() async {
+   final pickedFile = await picker.getImage(source: ImageSource.camera);
 
-    setState(() {
-      if (pickedFile != null) {
-        _image = File(pickedFile.path);
-      } else {
-        print('No image selected.');
-      }
-    });
-  }
+   setState(() {
+     _image = File(pickedFile.path);
+   });
+ }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Image Picker Example'),
-      ),
-      body: Center(
-        child: _image == null
-            ? Text('No image selected.')
-            : Image.file(_image),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: getImage,
-        tooltip: 'Pick Image',
-        child: Icon(Icons.add_a_photo),
-      ),
-    );
-  }
+ Future getImageFromGallery() async {
+   final pickedFile = await picker.getImage(source: ImageSource.gallery);
+
+   setState(() {
+     _image = File(pickedFile.path);
+   });
+ }
+
+ @override
+ Widget build(BuildContext context) {
+   return Scaffold(
+     appBar: AppBar(
+       title: Text('Image Picker Demo'),
+     ),
+     body: Center(
+       child: Column(
+         crossAxisAlignment: CrossAxisAlignment.center,
+         children: [
+           Padding(
+             padding: const EdgeInsets.all(8.0),
+             child: Container(
+                 width: 300,
+                 child: _image == null
+                     ? Text('No image selected.')
+                     : Image.file(_image)),
+           ),
+           Row(
+             mainAxisAlignment: MainAxisAlignment.spaceAround,
+             children: [
+               FloatingActionButton(
+                 onPressed: getImageFromCamera,
+                 tooltip: 'Pick Image From Camera',
+                 child: Icon(Icons.add_a_photo),
+               ),
+               FloatingActionButton(
+                 onPressed: getImageFromGallery,
+                 tooltip: 'Pick Image From Gallery',
+                 child: Icon(Icons.photo_library),
+               ),
+             ],
+           )
+         ],
+       ),
+     ),
+   );
+ }
 }
