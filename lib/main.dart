4da807cecdf52043bb05d008ae3058d0dfa68f37
Y.Yuna <he11o.2106y@gmail.com>
2020-11-19 19:37:09 +0900

    no message

--- lib/main.dart
+++ lib/main.dart
@@ -1,175 +1,277 @@
-import 'dart:async';
-import 'dart:io';
-import 'package:camera/camera.dart';
+// Copyright 2018 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:path/path.dart' show join;
-import 'package:path_provider/path_provider.dart';
 
-Future<void> main() async {
-  // runAppが実行される前に、cameraプラグインを初期化
-  WidgetsFlutterBinding.ensureInitialized();
 
-  // デバイスで使用可能なカメラの一覧を取得する
-  final cameras = await availableCameras();
-
-  // 利用可能なカメラの一覧から、指定のカメラを取得する
-  final firstCamera = cameras.first;
-
-  runApp(MyApp(camera: firstCamera));
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  final CameraDescription camera;
-  const MyApp({Key key, @required this.camera}) : super(key: key);
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(
-        camera: camera,
-      ),
+      home: TopPage(),
+      routes: <String, WidgetBuilder> {
+        '/top': (BuildContext context) => new TopPage(),
+        '/Main': (BuildContext context) => new MainPage(),
+      },
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final CameraDescription camera;
-  const MyHomePage({Key key, @required this.camera}) : super(key: key);
+
+class TopPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Camera Example'),
-      ),
-      body: Wrap(
-        children: [
-          RaisedButton(
-              child: const Text('Camera'),
-              onPressed: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context) {
-                  return CameraHome(camera: camera,);
-                }));
-              }),
+      backgroundColor: Colors.amber[300],
+      body:
+      new Stack(
+
+        children:<Widget>[
+          new Image.asset('images/main.png',
+            // width: 420,
+            fit:BoxFit.cover,
+          ),
+          new Align(
+            alignment: new Alignment(0.0, 1.0),
+
+            child: Container(
+              margin: new EdgeInsets.only(bottom:195.0),
+              child: new SizedBox(
+                width:200,
+                height:50,
+                child:new RaisedButton(
+                  shape:  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  color: Colors.white,
+
+                  // materialTapTargetSize: MaterialTapTargetSize(),
+                  onPressed:() => Navigator.of(context).pushNamed("/Main"),
+
+                  child:Text( "  スタート  ",
+                    style: TextStyle(
+                      color: Colors.deepOrange,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
         ],
       ),
     );
   }
 }
 
-class CameraHome extends StatefulWidget {
-  final CameraDescription camera;
 
-  const CameraHome({Key key, @required this.camera}) : super(key: key);
 
+class MainPage extends StatelessWidget {
   @override
-  State<StatefulWidget> createState() => CameraHomeState();
-}
 
-class CameraHomeState extends State<CameraHome> {
-  // デバイスのカメラを制御するコントローラ
-  CameraController _cameraController;
-  // コントローラーに設定されたカメラを初期化する関数
-  Future<void> _initializeCameraController;
-
-  @override
-  void initState() {
-    super.initState();
-
-    // コントローラを初期化
-    _cameraController = CameraController(
-        // 使用するカメラをコントローラに設定
-        widget.camera,
-        // 使用する解像度を設定
-        // low : 352x288 on iOS, 240p (320x240) on Android
-        // medium : 480p (640x480 on iOS, 720x480 on Android)
-        // high : 720p (1280x720)
-        // veryHigh : 1080p (1920x1080)
-        // ultraHigh : 2160p (3840x2160)
-        // max : 利用可能な最大の解像度
-        ResolutionPreset.max);
-    // コントローラーに設定されたカメラを初期化
-    _initializeCameraController = _cameraController.initialize();
-  }
-
-  @override
-  void dispose() {
-    // ウィジェットが破棄されたタイミングで、カメラのコントローラを破棄する
-    _cameraController.dispose();
-    super.dispose();
-  }
-
-  @override
+//appbarの関数化　戻るボタンの遷移場所の変数
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
-        title: const Text(''),
-      ),
-      // FutureBuilderを実装
-      body: FutureBuilder<void>(
-        future: _initializeCameraController,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.done) {
-            // カメラの初期化が完了したら、プレビューを表示
-            return CameraPreview(_cameraController);
-          } else {
-            // カメラの初期化中はインジケーターを表示
-            return const Center(child: CircularProgressIndicator());
-          }
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.camera_alt),
-        // ボタンが押下された際の処理
-        onPressed: () async {
-          try {
-
-            // 画像を保存するパスを作成する
-            final path = join(
-              (await getApplicationDocumentsDirectory()).path,
-              '${DateTime.now()}.png',
-            );
-
-            // カメラで画像を撮影する
-            await _cameraController.takePicture(path);
-
-            // 画像を表示する画面に遷移
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => CameraDisplay(imgPath: path),
+        backgroundColor: Colors.orange[400],
+        centerTitle: true,
+        title:  RichText(
+          text:TextSpan(
+            style: TextStyle(
+              color: Colors.white,
+              letterSpacing: 4.0,
+              //fontStyle: FontStyle.italic,
+              fontWeight: FontWeight.bold,
+              fontSize: 20,
+              fontFamily: "Pacifico",
+            ),
+            children: [
+              TextSpan(
+                  text: '  ぬりえ '
               ),
-            );
-          } catch (e) {
-            print(e);
-          }
-        },
+              TextSpan(
+                  text: 'de',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontStyle: FontStyle.italic,
+                    letterSpacing: 4.0,
+                    fontSize: 30,
+                  )
+
+              ),
+              WidgetSpan(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 1.0),
+                  child: Icon(Icons.brush_outlined),
+                ),
+              ),
+              TextSpan(text: ' GO'),
+            ],
+
+          ),
+        ),
       ),
+
+      body:
+      Column(
+
+        children:<Widget>[
+          Container(
+            margin: const EdgeInsets.all(5),
+
+            child:Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                //ぬりえのアイコン
+
+                SizedBox.fromSize(
+
+                  size: Size(400, 190), // button width and height
+
+                  //child: ClipOval(
+                  child: Material(
+                    color:  Colors.pink[200],// button color
+                    child: InkWell(
+                      splashColor: Colors.red, // splash color
+                      onTap:  () => Navigator.of(context).pushNamed("/subpage"), // button pressed
+                      //遷移先のクラス名（ぬりえへ）
+
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+
+                        children: <Widget>[
+                          Icon(Icons.color_lens_outlined,color: Colors.black,size: 80), // icon
+                          Text("ぬりえ",
+                            style:  TextStyle(
+                              //fontWeight: FontWeight.bold,
+                              color: Colors.black,
+                              fontSize: 20,
+                            ),
+                          ), // text
+                        ],
+                      ),
+                      // ),
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.all(5),
+                ),
+
+                SizedBox.fromSize(
+                  size: Size(400, 190), // button width and height
+                  //child: ClipOval(
+                  child: Material(
+                    color: Colors.deepOrangeAccent, // button color
+                    child: InkWell(
+                      splashColor: Colors.orange[200], // splash color
+   //                   onTap:  () => Navigator.of(context).pushNamed("/subpage"),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Icon(Icons.photo_library,color: Colors.black,size: 80),
+                          Text("かこのぬりえ",
+                            style: TextStyle(
+                              fontSize: 20,
+                            ),), // text
+                        ],
+                      ),
+                    ),
+                    // ),
+                  ),
+                ),
+/*
+                    Container(
+                      margin: EdgeInsets.all(40),
+                    ),
+
+ */
+                Container(
+                  margin: EdgeInsets.all(5),
+                ),
+                SizedBox.fromSize(
+                  size: Size(400, 190), // button width and height
+                  //  child: ClipOval(
+                  child: Material(
+                    color: Colors.lightBlueAccent, // button color
+                    child: InkWell(
+                      splashColor: Colors.blue, // splash color
+                      onTap:  () => Navigator.of(context).pushNamed("/subpage"), // button pressed
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Icon(Icons.photo_camera_outlined,color: Colors.black,size: 80),// icon
+                          Text("いろあつめ",
+                            style: TextStyle(
+                              fontSize: 20,
+                            ),), // text
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+
     );
   }
 }
 
-class CameraDisplay extends StatelessWidget {
-  // 表示する画像のパス
-  final String imgPath;
 
-  // 画面のコンストラクタ
-  const CameraDisplay({Key key, this.imgPath}) : super(key: key);
+void app(){
 
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Picture'),
+        backgroundColor: Colors.orange,
+        centerTitle: true,
+        actions: <Widget>[
+          IconButton(
+            icon:Icon(Icons.arrow_back_rounded),
+            onPressed:()=> Navigator.of(context).pushNamed("/main"),
+          ),
+        ],
+
+        title:  RichText(
+          text:TextSpan(
+            style: TextStyle(
+              color: Colors.white,
+              letterSpacing: 4.0,
+              fontStyle: FontStyle.italic,
+              fontWeight: FontWeight.bold,
+              fontSize: 20,
+            ),
+            children: [
+              TextSpan(
+                  text: '  ぬりえ '
+              ),
+              TextSpan(
+                  text: 'de',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontStyle: FontStyle.italic,
+                    letterSpacing: 4.0,
+                    fontSize: 30,
+                  )
+              ),
+              TextSpan(text: ' GO'),
+            ],
+
+          ),
+        ),
       ),
-      body: Column(
-        // Imageウィジェットで画像を表示する
-        children: [Expanded(child: Image.file(File(imgPath)))],
-      )
     );
   }
 }
